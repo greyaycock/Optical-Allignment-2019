3df557ed3410193d583438b0acd748f8f679b0ae
greyaycock <52426167+greyaycock@users.noreply.github.com>
2019-07-14 19:47:21 -0400

    Add files via upload

--- Image.py
+++ Image.py
@@ -5,12 +5,13 @@ class Image:
     height, width = 0, 0
     
     def __init__(self, heightInput, widthInput, pixelValue):
-        height = heightInput
-        width = widthInput
+        self.height = heightInput
+        self.width = widthInput
+        self.pixelValue = pixelValue
         
-        n = np.full((width, height), pixelValue)        #Creates a numpy object and creates the specified array
+        n = np.full((self.width, self.height), self.pixelValue)        #Creates a numpy object and creates the specified array
         hdu = pyfits.PrimaryHDU(n)                      #PrimaryHDU object to encapsulate the data
-        hdulist = pyfits.HDUList([hdu])                 #Creating an HDUList to contain the newly created primary HDU
+        self.hdulist = pyfits.HDUList([hdu])                 #Creating an HDUList to contain the newly created primary HDU
     
     def readPixel(self):
         print('')
@@ -18,13 +19,14 @@ class Image:
         print('')
         
     def saveImage(self, imageName):
-        hdulist.writeto(imageName)                      #Should save file (and was in previous versions),
+        self.hdulist.writeto(imageName)                      #Should save file (and did in previous versions),
                                                         #yet is now not due to the saveImage method not having access to 'hdulist'
 
 
 
 ###Just the code I've been using to test as I go###
 obj = Image(50, 50, 100.0)
+obj.saveImage('test.fits')
 print(obj.height, obj.width)
 
         
