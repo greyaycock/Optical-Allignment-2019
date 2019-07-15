9fd49ef5ebbeed47beab2ce478660d941ed35293
greyaycock <52426167+greyaycock@users.noreply.github.com>
2019-07-15 18:04:29 -0400

    Add files via upload

--- Image.py
+++ Image.py
@@ -2,31 +2,33 @@ import pyfits
 import numpy as np
 
 class Image:
-    height, width = 0, 0
     
     def __init__(self, heightInput, widthInput, pixelValue):
         self.height = heightInput
         self.width = widthInput
         self.pixelValue = pixelValue
-        
-        n = np.full((self.width, self.height), self.pixelValue)         #Creates a numpy object and creates the specified array
-        hdu = pyfits.PrimaryHDU(n)                                      #PrimaryHDU object to encapsulate the data
-        self.hdulist = pyfits.HDUList([hdu])                            #Creating an HDUList to contain the newly created primary HDU
+        self.n = np.full((self.width, self.height), self.pixelValue)                 #Creates a numpy object and creates the specified array
     
-    def readPixel(self):                                                #Work in progress
-        print('')
-    def writePixel(self):
-        print('')
+    def readPixel(self, pixelX, pixelY):                                             #Work in progress
+        print(self.n[pixelX, pixelY])
+        
+    def writePixel(self, pixelX, pixelY, newValue):
+        self.n[pixelX, pixelY] = newValue                                            #SHOULD just change the pixel at [pixelX, pixelY] to newValue
+        print("Pixel at (" , pixelX , "," , pixelY , ") changed to " , newValue , ".")
+        
         
     def saveImage(self, imageName):
-        self.hdulist.writeto(imageName)                                 #Saves file as imageName parameter (MUST INCLUDE '.fits' extension!)
-                                                        
+        hdu = pyfits.PrimaryHDU(self.n)                                     #PrimaryHDU object to encapsulate the data
+        self.hdulist = pyfits.HDUList([hdu])                                #Creating an HDUList to contain the newly created primary HDU
+        self.hdulist.writeto(imageName)                                     #Saves file as imageName parameter (MUST INCLUDE '.fits' extension!)
 
 
 
 ###Just the code I've been using to test as I go###
 obj = Image(50, 50, 100.0)
-obj.saveImage('test.fits')
+obj.readPixel(3, 3)
+obj.writePixel(35, 35, 50.0)
+obj.saveImage('test17.fits')
 print(obj.height, obj.width)
 
         
