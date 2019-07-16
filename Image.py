dd8397be0c8f833e8e60a940688de6d302c269da
greyaycock <52426167+greyaycock@users.noreply.github.com>
2019-07-15 21:52:31 -0400

    Add files via upload

--- Image.py
+++ Image.py
@@ -8,6 +8,10 @@ class Image:
         self.width = widthInput
         self.pixelValue = pixelValue
         self.n = np.full((self.width, self.height), self.pixelValue)                 #Creates a numpy object and creates the specified array
+        
+    def __init__(self, imageInput):
+        self.hdulist = pyfits.open(imageInput)                                       #Opens a FITS image as a pyfits object
+        self.n = self.hdulist[0].data                                                #Reads the first (and only) HDU, which contains the pixel array
     
     def readPixel(self, pixelX, pixelY):                                             #Work in progress
         print(self.n[pixelX, pixelY])
@@ -25,11 +29,12 @@ class Image:
 
 
 ###Just the code I've been using to test as I go###
-obj = Image(50, 50, 100.0)
+#obj = Image(50, 50, 100.0)
+obj = Image('image016.fits')
 obj.readPixel(3, 3)
 obj.writePixel(35, 35, 50.0)
-obj.saveImage('test17.fits')
-print(obj.height, obj.width)
+obj.saveImage('test19.fits')
+#print(obj.height, obj.width)
 
         
     
