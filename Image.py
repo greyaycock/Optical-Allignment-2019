c77086e16b7c206f60a324894684951fc36af668
greyaycock <52426167+greyaycock@users.noreply.github.com>
2019-07-20 14:11:23 -0400

    Add files via upload

--- Image.py
+++ Image.py
@@ -1,9 +1,10 @@
 import pyfits
 import numpy as np
+import time
 
 class Image:
     
-    def __init__(self, heightInput, widthInput, pixelValue):
+    def __init__(self, heightInput, widthInput, pixelValue):                         #Constructor
         self.height = heightInput
         self.width = widthInput
         self.pixelValue = pixelValue
@@ -14,78 +15,52 @@ class Image:
         self.n = self.hdulist[0].data                                                #Reads the first (and only) HDU, which contains the pixel array
     
     def readPixel(self, pixelX, pixelY):                                             #Work in progress
-        print(self.n[pixelX, pixelY])
+        return self.n[pixelX, pixelY]
         
     def writePixel(self, pixelY, pixelX, newValue):
         self.n[pixelX, pixelY] = newValue                                            #SHOULD just change the pixel at [pixelX, pixelY] to newValue
         print("Pixel at (" , pixelX , "," , pixelY , ") changed to " , newValue , ".")
+      
+#######################Circle Drawing############################      
         
-    def drawCircle(self, centerX, centerY):
-        self.writePixel(centerX, centerY, 50.0)
-        self.writePixel(centerX + 10, centerY, 50.0)
-        self.writePixel(centerX + 10, centerY+1, 50.0)                                    #Method to draw a circle with a radius 10 on the image
-        self.writePixel(centerX + 9, centerY+2, 50.0)
-        self.writePixel(centerX + 9, centerY+3, 50.0)
-        self.writePixel(centerX + 8, centerY+4, 50.0)
-        self.writePixel(centerX + 8, centerY+5, 50.0)
-        self.writePixel(centerX + 7, centerY+6, 50.0)
-        self.writePixel(centerX + 6, centerY+7, 50.0)
-        self.writePixel(centerX + 5, centerY+7, 50.0)
-        self.writePixel(centerX + 4, centerY+8, 50.0)
-        self.writePixel(centerX + 3, centerY+8, 50.0)       
-        self.writePixel(centerX + 2, centerY+9, 50.0)
-        self.writePixel(centerX + 1, centerY+9, 50.0)
-        self.writePixel(centerX + 0, centerY+9, 50.0)
-        self.writePixel(centerX + -1, centerY+9, 50.0)
-        self.writePixel(centerX + -2, centerY+8, 50.0)
-        self.writePixel(centerX + -3, centerY+8, 50.0)
-        self.writePixel(centerX + -4, centerY+7, 50.0)
-        self.writePixel(centerX + -5, centerY+7, 50.0)
-        self.writePixel(centerX + -6, centerY+6, 50.0)
-        self.writePixel(centerX + -7, centerY+5, 50.0)
-        self.writePixel(centerX + -7, centerY+4, 50.0)
-        self.writePixel(centerX + -8, centerY+3, 50.0)
-        self.writePixel(centerX + -8, centerY+2, 50.0)
-        self.writePixel(centerX + -9, centerY+1, 50.0)
-        self.writePixel(centerX + -9, centerY, 50.0)
-        self.writePixel(centerX + -9, centerY-1, 50.0)
-        self.writePixel(centerX + -9, centerY-2, 50.0)
-        self.writePixel(centerX + -8, centerY-3, 50.0)
-        self.writePixel(centerX + -8, centerY-4, 50.0)
-        self.writePixel(centerX + -7, centerY-5, 50.0)
-        self.writePixel(centerX + -7, centerY-6, 50.0)
-        self.writePixel(centerX + -6, centerY-7, 50.0)
-        self.writePixel(centerX + -5, centerY-8, 50.0)
-        self.writePixel(centerX + -4, centerY-8, 50.0)
-        self.writePixel(centerX + -3, centerY-9, 50.0)
-        self.writePixel(centerX + -2, centerY-9, 50.0)
-        self.writePixel(centerX + -1, centerY-10, 50.0)
-        self.writePixel(centerX + 0, centerY-10, 50.0)
-        self.writePixel(centerX + 1, centerY-10, 50.0)
-        self.writePixel(centerX + 2, centerY-10, 50.0)
-        self.writePixel(centerX + 3, centerY-9, 50.0)
-        self.writePixel(centerX + 4, centerY-9, 50.0)
-        self.writePixel(centerX + 5, centerY-8, 50.0)
-        self.writePixel(centerX + 6, centerY-8, 50.0)
-        self.writePixel(centerX + 7, centerY-7, 50.0)
-        self.writePixel(centerX + 8, centerY-6, 50.0)
-        self.writePixel(centerX + 8, centerY-5, 50.0)
-        self.writePixel(centerX + 9, centerY-4, 50.0)
-        self.writePixel(centerX + 9, centerY-3, 50.0)
-        self.writePixel(centerX + 10, centerY-2, 50.0)
-        self.writePixel(centerX + 10, centerY-1, 50.0)
+    def drawCircle(self, centerX, centerY, x, y):                                    #Method responsible for the actual pixel writing
         
-           
+        self.writePixel(centerX + x, centerY - y, 50.0)                              #These 6 writePixel() calls create 6/8 of the circle outline,
+        self.writePixel(centerX - x, centerY - y, 50.0)                              #with each call being responsible for one octile of the circle.
+        self.writePixel(centerX + y, centerY + x, 50.0)                              #The result is a "bowl" shape, leaving the top two octile
+        self.writePixel(centerX - y, centerY + x, 50.0)                              #empty so that the loops following can go through and fill in the rest.
+        self.writePixel(centerX + y, centerY - x, 50.0)
+        self.writePixel(centerX - y, centerY - x, 50.0)
         
+        for xx in range(centerX - x, centerX + x + 1):                               #This loop filles in the top and bottom quarters of the circle.
+            self.writePixel(xx, centerY + y, 50.0)
+            self.writePixel(xx, centerY - y, 50.0)
+        for xx in range(centerX - y, centerX + y):                                   #This loop fills in the middle quarters of the circle.
+            self.writePixel(xx, centerY + x, 50.0)
+            self.writePixel(xx, centerY - x, 50.0)
+
+    def generateCircle(self, xc, yc, r):                                   #Method based on Bresenham's circle-generation algorithm
+        x = 0                                                              #Takes parameters for center (xc, yc) coordinate, as well as the radius (r)
+        y = r
+        d = 3 - (2*r)
+        self.drawCircle(xc, yc, x, y)
+        while(y>=x):
+            x+=1
+            if(d>0):                                                       #Updates d and y values
+                y-=1
+                d = d+4*(x-y)+10
+            else:
+                d = d + 4 * x + 6
+            self.drawCircle(xc, yc, x, y)
+            time.sleep(.06)                                                #Delay helps program run smoother
+            
+################File Manipulation##########################        
     def saveImage(self, imageName):
-        hdu = pyfits.PrimaryHDU(self.n)                                     #PrimaryHDU object to encapsulate the data
-        self.hdulist = pyfits.HDUList([hdu])                                #Creating an HDUList to contain the newly created primary HDU
-        self.hdulist.writeto(imageName)                                     #Saves file as imageName parameter (MUST INCLUDE '.fits' extension!)
-
-
-
-###Just the code I've been using to test as I go###
+        hdu = pyfits.PrimaryHDU(self.n)                                    #PrimaryHDU object to encapsulate the data
+        self.hdulist = pyfits.HDUList([hdu])                               #Creating an HDUList to contain the newly created primary HDU
+        self.hdulist.writeto(imageName)                                    #Saves file as imageName parameter (MUST INCLUDE '.fits' extension!)
+        
+################Test Code##################################
 obj = Image(50, 50, 51.0)
-obj.drawCircle(35, 35)
-obj.saveImage('test43.fits')
-
+obj.generateCircle(35, 35, 10)
+obj.saveImage('circleTest13.fits')
