830b7547c8feea83c4c4db2558dacab68c6510d3
greyaycock <52426167+greyaycock@users.noreply.github.com>
2019-08-05 16:38:48 -0400

    Add files via upload

--- OpticalModel.py
+++ OpticalModel.py
@@ -1,5 +1,6 @@
 from Image import *
 import math 
+import numpy as np
 
 
 class OpticalModel:
@@ -13,17 +14,31 @@ class OpticalModel:
     def getDonut(self):
         
         image = Image(self.height, self.width, 0.0)                                     #Creates an image object
-        image.generateCircle(self.xc,self.yc,self.starDiameter,100.0)                   #Generates outer star-circle
+        image.generateCircle(self.xc,self.yc,self.starDiameter/2,100.0)                 #Generates outer star-circle
 
 #################Calculations##############
         xoffset = self.tiltOffset * math.sin(self.tiltAngle)                            #Calculates x-direction change based on tilt offset and angle
         yoffset = self.tiltOffset * math.cos(self.tiltAngle)                            #Calculates y-direction change based on tilt offseta and angle
-
-###########Applying calculations###########
-        image.generateCircle(self.xc+xoffset, self.yc+yoffset, 5, 0.0)                  #Generates inner star-circle
+        innerRadius = self.holeSize*(self.starDiameter/2)
+        image.generateCircle(self.xc+xoffset, self.yc+yoffset, innerRadius, 0.0)        #Generates inner circle
         
-        image.saveImage('OpticalModelTest.fits')
+#######Caclulating and applying flux#######     
+        totalValue = 0                                                                  
+        for x in range(0, self.width):                                                  #Cycles through every pixel and adds the values up
+            for y in range(0, self.height):
+                totalValue += image.readPixel(x, y)
+                
+        fluxRatio = self.totalFlux / totalValue                                         #Calculates the modifier that every element will be multiplied by
+        
+        for x in range(0, self.width):                                                  #Cycles through every element and applies flux change
+            for y in range(0, self.height):
+                if (image.readPixel(x, y) != 0.0) and (image.readPixel(x, y) >0.0):
+                    newValue = image.readPixel(x, y) * fluxRatio
+                    image.writePixel(y, x, newValue)
+                
+        
+        image.saveImage('OpticalModelTest1.fits')
         
 ####################Test code###############
-opti = OpticalModel(90, 90, 10, 50.0, 50.0, 10, 2.7, math.pi, 10)                       #An OpticalModel object with the "typical" values outlined in 
+opti = OpticalModel(90, 90, 0.35, 50.0, 50.0, 20, 2.7, math.pi/2, 10000)                  #An OpticalModel object with the "typical" values outlined in 
 opti.getDonut()                                                                         #ProjectPackageA2, as well as a tilt angle of pi (-y direction)
